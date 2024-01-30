6279ca2785958e0479492586209a986a368a98e9
sogdian <sogdian4815@gmail.com>
2024-01-30 22:31:45 +0100

    отобразил 6 карточек на странице

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCards() {
+  const cardTemplate = document.querySelector('#card-template').content;
+
+  initialCards.forEach((item) => {
+    const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+    placesItem.querySelector('.card__image').src = item.link;
+    placesItem.querySelector('.card__image').alt = item.name;
+
+    placesList.append(placesItem);
+  });
+}
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+addCards();
