f246967ecded75d05b988d105078f4d88ba9cb7a
sogdian <sogdian4815@gmail.com>
2024-01-31 22:01:43 +0100

    заработало

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,31 @@
 const placesList = document.querySelector('.places__list');
-const listItems = document.querySelectorAll('.places__item');
 
-function addCards(initialCards, removeCard) {
+function addCard(initialCard, removeCardCallback) {
   const cardTemplate = document.querySelector('#card-template').content;
   const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
   const cardDeleteButton = placesItem.querySelector('.card__delete-button');
 
-  placesItem.querySelector('.card__image').src = initialCards.link;
-  placesItem.querySelector('.card__image').alt = initialCards.name;
+  placesItem.querySelector('.card__image').src = initialCard.link;
+  placesItem.querySelector('.card__image').alt = initialCard.name;
 
-  // cardDeleteButton.addEventListener('click', removeCard);
+  function removeCard() {
+    // placesItem.remove();
+    // console.log("13 placesItem.remove()")
+    // Вызывайте колбэк только после удаления карточки
+    removeCardCallback(placesItem);
+    // console.log("16 removeCardCallback")
+  }
 
-  cardDeleteButton.addEventListener('click', function(){
-    placesItem.remove();
-  });
+  cardDeleteButton.addEventListener('click', removeCard);
 
   placesList.append(placesItem);
 }
 
-function removeCard() {
-  // listItems.remove();
+const removeCard = (item) => {
+  item.remove();
+  // console.log("25 removeCard()")
 }
 
 initialCards.forEach((item) => {
-  addCards(item, removeCard);
+  addCard(item, removeCard);
 });
