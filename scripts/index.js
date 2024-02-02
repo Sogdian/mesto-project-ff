6762d14f669a86cc44eb532678534b754eb0c9db
sogdian <sogdian4815@gmail.com>
2024-02-02 14:54:03 +0100

    добавил название карточки

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,14 @@ const placesList = document.querySelector('.places__list');
 
 function createCard(initialCard, removeCardCallback) {
   const cardTemplate = document.querySelector('#card-template').content;
-  const placesItem = cardTemplate
-    .querySelector('.places__item')
-    .cloneNode(true);
+  const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardTitle = placesItem.querySelector('.card__title');
   const cardDeleteButton = placesItem.querySelector('.card__delete-button');
 
   placesItem.querySelector('.card__image').src = initialCard.link;
   placesItem.querySelector('.card__image').alt = initialCard.name;
+  cardTitle.textContent = initialCard.name;
+
 
   function removeCard() {
     removeCardCallback(placesItem);
