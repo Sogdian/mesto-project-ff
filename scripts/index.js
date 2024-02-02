e68e81522be31d11612148bd1a9be290dacb1a2e
sogdian <sogdian4815@gmail.com>
2024-02-02 14:49:02 +0100

    фикс после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
 const placesList = document.querySelector('.places__list');
 
-function addCard(initialCard, removeCardCallback) {
+function createCard(initialCard, removeCardCallback) {
   const cardTemplate = document.querySelector('#card-template').content;
-  const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const placesItem = cardTemplate
+    .querySelector('.places__item')
+    .cloneNode(true);
   const cardDeleteButton = placesItem.querySelector('.card__delete-button');
 
   placesItem.querySelector('.card__image').src = initialCard.link;
@@ -14,13 +16,18 @@ function addCard(initialCard, removeCardCallback) {
 
   cardDeleteButton.addEventListener('click', removeCard);
 
-  placesList.append(placesItem);
+  return placesItem;
 }
 
 const removeCard = (item) => {
   item.remove();
 };
 
+function addCard(item) {
+  let placesItem = createCard(item, removeCard);
+  placesList.append(placesItem);
+}
+
 initialCards.forEach((item) => {
-  addCard(item, removeCard);
+  addCard(item);
 });
