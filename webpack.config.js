4952382cf91158daf6aa3d2f4a1862459d16a8b8
sogdian <sogdian4815@gmail.com>
2024-02-14 21:22:13 +0100

    next

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // подключите к проекту mini-css-extract-plugin
+
+module.exports = {
+	entry: { main: './src/index.js' }, //точка входа
+	output: {
+		path: path.resolve(__dirname, 'dist'), //точка выхода
+		filename: 'main.js',
+		publicPath: ''
+	},
+	mode: 'development', // добавили режим разработчика
+	devServer: { //настройки локального сервера:
+		static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+		compress: true, // это ускорит загрузку в режиме разработки
+		port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+
+		open: true // сайт будет открываться сам при запуске npm run dev
+	},
+	module: {
+		rules: [ // rules — это массив правил
+			// добавим в него объект правил для бабеля
+			{
+				// регулярное выражение, которое ищет все js файлы
+				test: /\.js$/,
+				// при обработке этих файлов нужно использовать babel-loader
+				use: 'babel-loader',
+				// исключает папку node_modules, файлы в ней обрабатывать не нужно
+				exclude: '/node_modules/'
+			},
+			// добавили правило для обработки файлов
+			{
+				// регулярное выражение, которое ищет все файлы с такими расширениями
+				test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+				type: 'asset/resource' //asset/resource позволяет переносить исходные файлы в конечную сборку в том же формате.
+			},
+			{
+				// применять это правило только к CSS-файлам
+				test: /\.css$/,
+				// при обработке этих файлов нужно использовать
+				// MiniCssExtractPlugin.loader и css-loader
+				use: [MiniCssExtractPlugin.loader, {
+					loader: 'css-loader',
+					options: { importLoaders: 1 } //https://webpack.js.org/loaders/css-loader/#importloaders
+						//Значение 1 говорит о том, что некоторые трансформации PostCSS нужно применить до css-loader.
+				},
+					'postcss-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		//https://webpack.js.org/plugins/html-webpack-plugin/#basic-usage
+		new HtmlWebpackPlugin({
+			template: './src/index.html' // путь к файлу index.html
+		}),
+		new CleanWebpackPlugin(), // использовали плагин
+		new MiniCssExtractPlugin() // подключение плагина для объединения файлов
+	]
+}
