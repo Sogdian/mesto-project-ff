76d7e04927c7dda7ef831e7dacfc8d3d41e42ef5
sogdian <sogdian4815@gmail.com>
2024-02-16 22:21:57 +0100

    отладка

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './vendor/fonts.css'; // добавьте импорт главного файла стилей
+import './vendor/normalize.css'; // добавьте импорт главного файла стилей
+import './images/add-icon.svg';
+import './images/avatar.jpg';
+import './images/card_1.jpg';
+import './images/card_2.jpg';
+import './images/card_3.jpg';
+import './images/close.svg';
+import './images/delete-icon.svg';
+import './images/edit-icon.svg';
+import './images/like-active.svg';
+import './images/like-inactive.svg';
+import './images/logo.svg';
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-const path = require('path'); // подключаем path к конфигу вебпак
+const path = require('path'); //утилита, которая превращает относительный путь в абсолютный
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // подключите к проекту mini-css-extract-plugin
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //плагин, который будет каждый раз при сборке проекта удалять содержимое папки dist.
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //для обработки css
 
 module.exports = {
 	entry: { main: './src/index.js' }, //точка входа
 	output: {
 		path: path.resolve(__dirname, 'dist'), //точка выхода
+			//dirname - эта переменная в Node.js доступна глобально. В ней хранится абсолютный путь до папки, в которой лежит файл, где мы используем эту переменную. В нашем случае абсолютный путь до папки с конфигом «Вебпака» — корневой папки нашего проекта.
 		filename: 'main.js',
 		publicPath: ''
 	},
@@ -15,34 +16,24 @@ module.exports = {
 		static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
 		compress: true, // это ускорит загрузку в режиме разработки
 		port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
-
 		open: true // сайт будет открываться сам при запуске npm run dev
 	},
 	module: {
 		rules: [ // rules — это массив правил
-			// добавим в него объект правил для бабеля
-			{
-				// регулярное выражение, которое ищет все js файлы
-				test: /\.js$/,
-				// при обработке этих файлов нужно использовать babel-loader
-				use: 'babel-loader',
-				// исключает папку node_modules, файлы в ней обрабатывать не нужно
-				exclude: '/node_modules/'
+			{ 		// добавим в него объект правил для бабеля
+				test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+				use: 'babel-loader', // при обработке этих файлов нужно использовать babel-loader
+				exclude: '/node_modules/' // исключает папку node_modules, файлы в ней обрабатывать не нужно
 			},
-			// добавили правило для обработки файлов
 			{
-				// регулярное выражение, которое ищет все файлы с такими расширениями
-				test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+				test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/, // регулярное выражение, которое ищет все файлы с такими расширениями
 				type: 'asset/resource' //asset/resource позволяет переносить исходные файлы в конечную сборку в том же формате.
 			},
 			{
-				// применять это правило только к CSS-файлам
-				test: /\.css$/,
-				// при обработке этих файлов нужно использовать
-				// MiniCssExtractPlugin.loader и css-loader
-				use: [MiniCssExtractPlugin.loader, {
+				test: /\.css$/, // применять это правило только к CSS-файлам
+				use: [MiniCssExtractPlugin.loader, { // при обработке этих файлов нужно использовать MiniCssExtractPlugin.loader и css-loader
 					loader: 'css-loader',
-					options: { importLoaders: 1 } //https://webpack.js.org/loaders/css-loader/#importloaders
+					options: { importLoaders: 1 } //Если вы используете директиву @import https://webpack.js.org/loaders/css-loader/#importloaders
 						//Значение 1 говорит о том, что некоторые трансформации PostCSS нужно применить до css-loader.
 				},
 					'postcss-loader'
@@ -52,10 +43,10 @@ module.exports = {
 	},
 	plugins: [
 		//https://webpack.js.org/plugins/html-webpack-plugin/#basic-usage
-		new HtmlWebpackPlugin({
+		new HtmlWebpackPlugin({ //HtmlWebpackPlugin - класс, с помощью которого можно конструировать объекты
 			template: './src/index.html' // путь к файлу index.html
 		}),
-		new CleanWebpackPlugin(), // использовали плагин
-		new MiniCssExtractPlugin() // подключение плагина для объединения файлов
+		new CleanWebpackPlugin(), // очищать директорию dist
+		new MiniCssExtractPlugin() //Это подключит плагин для объединения css-файлов
 	]
 }
