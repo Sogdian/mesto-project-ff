e2eca47d99ee036ef24a22dd4f23e7fc1e05285a
sogdian <sogdian4815@gmail.com>
2024-02-18 19:01:27 +0100

    Весь п. 4

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -8,27 +8,43 @@ const popupTypeImage = document.querySelector('.popup_type_image');
 const popupClose = document.querySelectorAll('.popup__close');
 const profileTitle = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
+const editProfile = document.querySelector('.edit-profile');
 
-const openTypeEditModal = () => {
+function openTypeEditModal() {
 	popupTypeEdit.style.display = 'flex'
-	document.forms.namedItem('edit-profile').elements.name.value = profileTitle.textContent;
-	document.forms.namedItem('edit-profile').elements.description.value = profileDescription.textContent;
-}
 
+	const name = document.forms.namedItem('edit-profile').elements.name;
+	name.value = profileTitle.textContent;
+	const description = document.forms.namedItem('edit-profile').elements.description;
+	description.value = profileDescription.textContent;
+
+	function handleFormSubmit(evt) {
+		saveNewTypeEditData(evt, name.value, description.value);
+	}
+
+	document.forms.namedItem('edit-profile').addEventListener('submit', handleFormSubmit);
+}
 profileEditButton.addEventListener('click', openTypeEditModal)
 
-const openTypeNewCardModal = () => {
-	popupTypeNewCard.style.display = 'flex'
+function saveNewTypeEditData(evt, name, description) {
+	evt.preventDefault()
+	profileTitle.textContent = name;
+	profileDescription.textContent = description;
+	closeModal();
 }
 
+function openTypeNewCardModal() {
+	popupTypeNewCard.style.display = 'flex'
+}
 profileAddButton.addEventListener('click', openTypeNewCardModal)
 
-const openTypeImageModal = (evt) => {
-	popupTypeImage.style.display = 'flex';
-	const eventTarget = evt.target;
-	popupImage.src = eventTarget.src;
+function openTypeImageModal(evt) {
+	if (evt.target.classList.contains('card__image')) {
+		popupTypeImage.style.display = 'flex';
+		const eventTarget = evt.target;
+		popupImage.src = eventTarget.src;
+	}
 };
-
 placesList.addEventListener('click', openTypeImageModal);
 
 // placesList.addEventListener('click', function (evt) {
@@ -37,24 +53,22 @@ placesList.addEventListener('click', openTypeImageModal);
 // 	}
 // });
 
-function closeModal () {
+function closeModal() {
 	popupTypeEdit.style.display = 'none'
 	popupTypeNewCard.style.display = 'none'
 	popupTypeImage.style.display = 'none'
 }
-
 popupClose.forEach((evt) => {
 	evt.addEventListener('click', closeModal)
 })
 
-function closeModalForESC (evt) {
+function closeModalWithEsc (evt) {
 	if(evt.key === 'Escape') {
 		closeModal();
 	}
 	document.removeEventListener('keydown', closeModal);
 }
-
-document.addEventListener('keydown', closeModalForESC);
+document.addEventListener('keydown', closeModalWithEsc);
 
 
 
