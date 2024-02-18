5062805e6113f141edcdb4252796cdd84283de0c
sogdian <sogdian4815@gmail.com>
2024-02-18 14:08:17 +0100

    рефактор

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,15 +1,40 @@
 const profileEditButton = document.querySelector('.profile__edit-button');
+const profileAddButton = document.querySelector('.profile__add-button');
+const placesList = document.querySelector('.places__list');
+const popupImage = document.querySelector('.popup__image');
 const popupTypeEdit = document.querySelector('.popup_type_edit');
 const popupTypeNewCard = document.querySelector('.popup_type_new-card');
 const popupTypeImage = document.querySelector('.popup_type_image');
-const profileAddButton = document.querySelector('.profile__add-button');
 const popupClose = document.querySelectorAll('.popup__close');
-const cardImage = document.querySelector('.card__image');
-const placesList = document.querySelector('.places__list');
-const placesItem = document.querySelectorAll('.places__item');
-const popupImage = document.querySelector('.popup__image');
 
-const closeModal = () => {
+const openTypeEditModal = () => {
+	popupTypeEdit.style.display = 'flex'
+}
+
+profileEditButton.addEventListener('click', openTypeEditModal)
+
+const openTypeNewCardModal = () => {
+	popupTypeNewCard.style.display = 'flex'
+}
+
+profileAddButton.addEventListener('click', openTypeNewCardModal)
+
+const openTypeImageModal = (evt) => {
+	popupTypeImage.style.display = 'flex';
+	const eventTarget = evt.target;
+	popupImage.src = eventTarget.src;
+};
+
+placesList.addEventListener('click', openTypeImageModal);
+
+// placesList.addEventListener('click', function (evt) {
+// 	if (evt.target.classList.contains('card__like-button')) {
+// 		evt.target.classList.toggle('button');
+// 	}
+// });
+
+
+function closeModal () {
 	popupTypeEdit.style.display = 'none'
 	popupTypeNewCard.style.display = 'none'
 	popupTypeImage.style.display = 'none'
@@ -19,36 +44,7 @@ popupClose.forEach((evt) => {
 	evt.addEventListener('click', closeModal)
 })
 
-popupTypeImage.addEventListener('click', closeModal);
-
-const openModal1 = () => {
-	popupTypeEdit.style.display = 'flex'
-}
-
-profileEditButton.addEventListener('click', openModal1)
-
-const openModal2 = () => {
-	popupTypeNewCard.style.display = 'flex'
-}
-
-profileAddButton.addEventListener('click', openModal2)
-
-const openModal3 = (evt) => {
-	console.log("!")
-	popupTypeImage.style.display = 'flex';
-	const eventTarget = evt.target;
-	popupImage.src = eventTarget.src;
-};
-
-placesList.addEventListener('click', openModal3);
-
-// placesList.addEventListener('click', function (evt) {
-// 	if (evt.target.classList.contains('card__like-button')) {
-// 		evt.target.classList.toggle('button');
-// 	}
-// });
-
-const closeModalForESC = (evt) => {
+function closeModalForESC (evt) {
 	if(evt.key === 'Escape') {
 		closeModal();
 	}
@@ -56,4 +52,6 @@ const closeModalForESC = (evt) => {
 }
 
 document.addEventListener('keydown', closeModalForESC);
+
+
 
