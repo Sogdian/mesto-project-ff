c91c61d146895ba0cb81b23af52eefc2e38a6e0d
sogdian <sogdian4815@gmail.com>
2024-02-19 18:31:56 +0100

    дебаг

--- src/index.js
+++ src/index.js
@@ -15,13 +15,13 @@ import './images/logo.svg';
 import {addCard} from "./scripts/cards";
 
 export const placesList = document.querySelector('.places__list');
-export const popupTypeImage = document.querySelector('.popup_type_image');
+// export const popupTypeImage = document.querySelector('.popup_type_image');
 export const popupImage = document.querySelector('.popup__image');
 export const popup = document.querySelector('.popup');
 export const profileEditButton = document.querySelector('.profile__edit-button');
 export const profileAddButton = document.querySelector('.profile__add-button');
-export const popupTypeEdit = document.querySelector('.popup_type_edit');
-export const popupTypeNewCard = document.querySelector('.popup_type_new-card');
+// export const popupTypeEdit = document.querySelector('.popup_type_edit');
+// export const popupTypeNewCard = document.querySelector('.popup_type_new-card');
 export const popupsClose = document.querySelectorAll('.popup__close');
 export const profileTitle = document.querySelector('.profile__title');
 export const profileDescription = document.querySelector('.profile__description');
